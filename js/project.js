cf97740f7da49b5a48748787a8023c64b142643c
vishwasshan <66668306+vishwasshan@users.noreply.github.com>
2020-09-11 21:11:18 +0530

    Add files via upload

---
+++ js/project.js
@@ -0,0 +1,10 @@
+// console.log("yes")
+// let hamberger=document.getElementById("hamberger")
+
+// hamberger.addEventListener("click", ()=>{
+//    let body= document.getElementById("body");
+//    body.style.backgroundColor="black";
+//    body.style.overflowY="hidden";
+   
+   
+// })
